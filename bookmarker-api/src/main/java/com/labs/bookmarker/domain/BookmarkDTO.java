464393a154d9d47482057853746769fd4eabc37b
gau-rao <gauraonandekar99@gmail.com>
2024-09-25 22:44:27 +0530

    github action support

---
+++ bookmarker-api/src/main/java/com/labs/bookmarker/domain/BookmarkDTO.java
@@ -0,0 +1,18 @@
+package com.labs.bookmarker.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookmarkDTO {
+    private Long id;
+    private String title;
+    private String url;
+    private Instant createdAt;
+}
---
+++ bookmarker-api/src/main/java/com/labs/bookmarker/domain/BookmarkRepository.java
@@ -0,0 +1,11 @@
+package com.labs.bookmarker.domain;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark , Long> {
+    @Query(" select new com.labs.bookmarker.domain.BookmarkDTO(b.id, b.title,b.url,b.createdAt) from Bookmark b")
+    Page<BookmarkDTO> findBookmark(Pageable pageable);
+}
